6b0867b7696e65bbb8c317bef70216d34a28d24b
code_klenn <kvborja@up.edu.ph>
2025-01-03 07:14:39 +0800

    init: added readmes

--- source_code/backend/server.js
+++ source_code/backend/server.js
@@ -1,10 +1,11 @@
 import express from 'express';
+import userRouter from './routers/userRouter.js';
 
+// initialize the express
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send("Hello!");
-});
+// initialize the routers (these are the things necessary for the endpoints)
+userRouter(app);
 
 app.listen(3000, () =>{
     console.log("Server started at port 3000");
