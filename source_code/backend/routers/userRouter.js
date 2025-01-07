a9c4d4d270621236aa18a58e923a5375027a190a
code_klenn <kvborja@up.edu.ph>
2025-01-07 23:02:12 +0800

    feat: connect to MongoDB

---
+++ source_code/backend/routers/userRouter.js
@@ -0,0 +1,8 @@
+
+import {} from '../controllers/userController.js'
+
+const userRouter = (app) => {
+
+};
+
+export default userRouter;
--- source_code/backend/server.js
+++ source_code/backend/server.js
@@ -1,5 +1,33 @@
 import express from 'express';
 import userRouter from './routers/userRouter.js';
+import dotenv from "dotenv";
+import { MongoClient, ServerApiVersion } from "mongodb";
+
+dotenv.config();
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(process.env.DB_URI, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        console.log("Waiting for connection to MongoDB...")
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. Server successfully connected to MongoDB");
+    } catch(error) {
+        console.error("Error connecting to MongoDB:", err);
+        process.exit(1); // Exit if the connection fails
+    }
+}
+
 
 // initialize the express
 const app = express();
@@ -7,6 +35,8 @@ const app = express();
 // initialize the routers (these are the things necessary for the endpoints)
 userRouter(app);
 
-app.listen(3000, () =>{
-    console.log("Server started at port 3000");
-})
+run().then(() =>{
+    app.listen(process.env.SERVER_PORT, () =>{
+        console.log(`Server started at ${process.env.SERVER_PORT}`);
+    })
+}).catch(console.dir);
