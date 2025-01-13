f18cd1fdfc4ebb7348c13a78a402e450bf108840
Myko Jefferson Javier <mmjavier5@up.edu.ph>
2025-01-13 05:39:59 -0500

    RE: Added resident-member branch, finished setting uprouter and function for retrieving all resmem data from db

--- source_code/backend/server.js
+++ source_code/backend/server.js
@@ -6,14 +6,13 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import mailer from "./utilities/mailer.js";
 import connectChat from "./utilities/connect_chat.js";
+import getAllResiRouter from "./routers/resident-membersRouter.js";
 dotenv.config();
 
-
 const uri = process.env.DB_URI;
-console.log(uri)
+console.log(uri);
 
 async function run() {
-
   try {
     console.log("Waiting for connection to MongoDB...");
     // mongoose connection (removed callbacks since latest version of Mongoose is no longer accepting callbacks)
@@ -23,7 +22,7 @@ async function run() {
     });
     console.log("Mongoose has successfully connected to MongoDB.");
     const verifyConnection = mongoose.connection;
-    
+
     // listen for errors
     verifyConnection.on("error", (err) =>
       console.log(`Connection error ${err}`)
@@ -46,6 +45,7 @@ app.use(bodyParser.json());
 
 // initialize the routers (these are the things necessary for the endpoints)
 userRouter(app);
+getAllResiRouter(app);
 
 // initialize utilities
 mailer(app);
@@ -60,5 +60,3 @@ run()
     });
   })
   .catch(console.dir);
-
-
