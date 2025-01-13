e19a2fb6f0c335cc285775599949bef0721177f2
pvgonzales <pvgonzales@up.edu.ph>
2025-01-13 23:13:59 +0800

    feat: added a router for toggleHeart

--- source_code/backend/routers/dashboardRouter.js
+++ source_code/backend/routers/dashboardRouter.js
@@ -1,8 +1,9 @@
-import { createPost, addComment, fetchPosts, updatePost, delPost } from '../controllers/dashboard.js';
+import { createPost, addComment, fetchPosts, updatePost, delPost, toggleHeart } from '../controllers/dashboard.js';
 
 const dashboardRouter = (app) => {
     app.post('/api/posts', createPost);
     app.post('/api/posts/:id/comment', addComment);
+    app.put('/api/posts/:id/heart', toggleHeart);
     app.get('/api/getposts', fetchPosts);
     app.put('/api/updatepost/:id', updatePost);
     app.delete('/api/delpost/:id', delPost);
