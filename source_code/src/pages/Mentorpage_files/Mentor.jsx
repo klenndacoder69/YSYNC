8af25ceeef80091037602721ffbb92b77a6444fb
MarcusOcampo <159239180+MarcusOcampo@users.noreply.github.com>
2025-01-16 21:11:14 +0800

    fix: component is now similar to the design in figma

--- source_code/src/pages/Mentorpage_files/Mentor.jsx
+++ source_code/src/pages/Mentorpage_files/Mentor.jsx
@@ -1,26 +1,29 @@
 import CardMentor from "./CardMentor.jsx"
 
 const mentors = [
-    {id: 1, image: "/assets/profile_face.png", name: "jessica soho", batch: "python", dept: "Scholastic", bio: "ako si jessica hehe", exp: "sigma", interests: "test muna"},
-    {id: 2, image: "/assets/profile-pic-2.png", name: "jessica soho", batch: "python", dept: "Scholastic", bio: "ako si jessica hehe", exp: "sigma", interests: "test muna"},
-    {id: 3, image: "/assets/profile.jpg", name: "jessica soho", batch: "python", dept: "Scholastic", bio: "ako si jessica hehe", exp: "sigma", interests: "test muna"}
+    {id: 1, image: "/assets/profile_face.png", name: "jessica soho", batch: "python", dept: "Scholastic", bio: "BIO GOES HERE", exp: "EXPERTISE GO HERE", interests: "INTERESTS GO HERE"},
+    {id: 1, image: "/assets/profile-pic-2.png", name: "jessica soho", batch: "python", dept: "Scholastic", bio: "BIO GOES HERE", exp: "EXPERTISE GO HERE", interests: "INTERESTS GO HERE"},
+    {id: 1, image: "/assets/profile.jpg", name: "jessica soho", batch: "python", dept: "Scholastic", bio: "BIO GOES HERE", exp: "EXPERTISE GO HERE", interests: "INTERESTS GO HERE"},
 ]
 
 export default function Mentor(){
     return(
         <>  
-            {mentors.map((mentor) => (
-                 <CardMentor
-                    key = {mentor.id}
-                    image = {mentor.image}
-                    name = {mentor.name}
-                    batch = {mentor.batch}
-                    dept = {mentor.dept}
-                    bio = {mentor.bio}
-                    exp = {mentor.exp}
-                    interests = {mentor.interests}
-                 />
-            ))}
+            <div style={{backgroundColor: '#132B41'}}>
+                {mentors.map((mentor) => (
+                    <CardMentor
+                        key = {mentor.id}
+                        image = {mentor.image}
+                        name = {mentor.name}
+                        batch = {mentor.batch}
+                        dept = {mentor.dept}
+                        bio = {mentor.bio}
+                        exp = {mentor.exp}
+                        interests = {mentor.interests}
+                    />
+                ))}
+            </div>
+            
         </>
     )
 }
