2ad605a5efc14636ff7e9bb200b9c747599b6ae7
MarcusOcampo <159239180+MarcusOcampo@users.noreply.github.com>
2025-01-16 19:53:33 +0800

    fix: instead of hover I changed to onclick

--- source_code/src/pages/Mentorpage_files/CardMentor.jsx
+++ source_code/src/pages/Mentorpage_files/CardMentor.jsx
@@ -1,28 +1,37 @@
-import "./CardMentor.css"
+import React, { useState } from "react";
+import "./CardMentor.css";
 
-export default function CardMentor({ image, name, batch, dept, bio, exp, interests }){
-    return(
-        <>
-            <div className="mentor-card-maincontainer">
-                <div className="mentor-card">
-                    <div className="mentor-card-front">
-                        <div className="mentor-card-image-wrapper"> 
-                            <img src={image} alt={`${name}'s profile`} className="mentor-card-image" />
-                        </div>
-                        <div className="mentor-card-front-info-wrapper">
-                            <h1 className="mentor-card-name">{name}</h1>
-                            <p className="mentor-card-batch">Batch {batch}</p>
-                            <p className="mentor-card-dept">{dept} Department</p>
-                        </div>
-                    </div>
+export default function CardMentor({ image, name, batch, dept, bio, exp, interests }) {
+  const [isFlipped, setIsFlipped] = useState(false); 
 
-                    <div className="mentor-card-back">
-                        <h2 className="mentor-card-bio">{bio}</h2>
-                        <h2 className="mentor-card-exp">{exp}</h2>
-                        <h2 className="mentor-card-interests">{interests}</h2>
-                    </div>
+  const handleCardClick = () => {
+    setIsFlipped(!isFlipped); //change the flip case
+  };
+
+  return (
+    <div className="mentor-card-maincontainer">
+        {/* the div below will get an additional className called mentor-card-flipped if the onClick happens which will activate the rotation for that className */}
+        <div className={`mentor-card ${isFlipped ? "mentor-card-flipped" : ""}`} onClick={handleCardClick}>
+        
+            <div className="mentor-card-front">
+                <div className="mentor-card-image-wrapper">
+                    <img src={image} alt={`${name}'s profile`} className="mentor-card-image"/>
+                </div>
+            
+                <div className="mentor-card-front-info-wrapper">
+                    <h1 className="mentor-card-name">{name}</h1>
+                    <p className="mentor-card-batch">Batch {batch}</p>
+                    <p className="mentor-card-dept">{dept} Department</p>
                 </div>
             </div>
-        </>
-    )
+
+            <div className="mentor-card-back">
+                <h2 className="mentor-card-bio">{bio}</h2>
+                <h2 className="mentor-card-exp">{exp}</h2>
+                <h2 className="mentor-card-interests">{interests}</h2>
+            </div>
+
+        </div>
+    </div>
+  );
 }
