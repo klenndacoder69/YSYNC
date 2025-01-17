cfc66a55ea76cb1cc117b5c26f0a27e189ecffc9
pvgonzales <pvgonzales@up.edu.ph>
2025-01-17 11:34:31 +0800

    feat: mentor choosing page

--- source_code/src/pages/Mentorpage_files/Mentor.jsx
+++ source_code/src/pages/Mentorpage_files/Mentor.jsx
@@ -1,29 +1,71 @@
 import CardMentor from "./CardMentor.jsx"
+import "./Mentor.css"
 
 const mentors = [
-    {id: 1, image: "/assets/profile_face.png", name: "jessica soho", batch: "python", dept: "Scholastic", bio: "BIO GOES HERE", exp: "EXPERTISE GO HERE", interests: "INTERESTS GO HERE"},
-    {id: 1, image: "/assets/profile-pic-2.png", name: "jessica soho", batch: "python", dept: "Scholastic", bio: "BIO GOES HERE", exp: "EXPERTISE GO HERE", interests: "INTERESTS GO HERE"},
-    {id: 1, image: "/assets/profile.jpg", name: "jessica soho", batch: "python", dept: "Scholastic", bio: "BIO GOES HERE", exp: "EXPERTISE GO HERE", interests: "INTERESTS GO HERE"},
+    {id: 1, image: "/assets/Snorlax.png", name: "POKEMON, SNORLAX B.", batch: "Hash", dept: "Scholastics", bio: "BIO GOES HERE", exp: "EXPERTISE GO HERE", interests: "INTERESTS GO HERE"},
+    {id: 1, image: "/assets/Snorlax.png", name: "POKEMON, SNORLAX B.", batch: "Hash", dept: "Scholastics", bio: "BIO GOES HERE", exp: "EXPERTISE GO HERE", interests: "INTERESTS GO HERE"},
+    {id: 1, image: "/assets/Snorlax.png", name: "POKEMON, SNORLAX B.", batch: "Hash", dept: "Scholastics", bio: "BIO GOES HERE", exp: "EXPERTISE GO HERE", interests: "INTERESTS GO HERE"},
+    {id: 1, image: "/assets/Snorlax.png", name: "POKEMON, SNORLAX B.", batch: "Hash", dept: "Scholastics", bio: "BIO GOES HERE", exp: "EXPERTISE GO HERE", interests: "INTERESTS GO HERE"},
+    {id: 1, image: "/assets/Snorlax.png", name: "POKEMON, SNORLAX B.", batch: "Hash", dept: "Scholastics", bio: "BIO GOES HERE", exp: "EXPERTISE GO HERE", interests: "INTERESTS GO HERE"},
+    {id: 1, image: "/assets/Snorlax.png", name: "POKEMON, SNORLAX B.", batch: "Hash", dept: "Scholastics", bio: "BIO GOES HERE", exp: "EXPERTISE GO HERE", interests: "INTERESTS GO HERE"},
+    {id: 1, image: "/assets/Snorlax.png", name: "POKEMON, SNORLAX B.", batch: "Hash", dept: "Scholastics", bio: "BIO GOES HERE", exp: "EXPERTISE GO HERE", interests: "INTERESTS GO HERE"},
+    {id: 1, image: "/assets/Snorlax.png", name: "POKEMON, SNORLAX B.", batch: "Hash", dept: "Scholastics", bio: "BIO GOES HERE", exp: "EXPERTISE GO HERE", interests: "INTERESTS GO HERE"},
+    {id: 1, image: "/assets/Snorlax.png", name: "POKEMON, SNORLAX B.", batch: "Hash", dept: "Scholastics", bio: "BIO GOES HERE", exp: "EXPERTISE GO HERE", interests: "INTERESTS GO HERE"},
+    {id: 1, image: "/assets/Snorlax.png", name: "POKEMON, SNORLAX B.", batch: "Hash", dept: "Scholastics", bio: "BIO GOES HERE", exp: "EXPERTISE GO HERE", interests: "INTERESTS GO HERE"},
+    {id: 1, image: "/assets/Snorlax.png", name: "POKEMON, SNORLAX B.", batch: "Hash", dept: "Scholastics", bio: "BIO GOES HERE", exp: "EXPERTISE GO HERE", interests: "INTERESTS GO HERE"},
+    {id: 1, image: "/assets/Snorlax.png", name: "POKEMON, SNORLAX B.", batch: "Hash", dept: "Scholastics", bio: "BIO GOES HERE", exp: "EXPERTISE GO HERE", interests: "INTERESTS GO HERE"},
+    {id: 1, image: "/assets/Snorlax.png", name: "POKEMON, SNORLAX B.", batch: "Hash", dept: "Scholastics", bio: "BIO GOES HERE", exp: "EXPERTISE GO HERE", interests: "INTERESTS GO HERE"},
+    {id: 1, image: "/assets/Snorlax.png", name: "POKEMON, SNORLAX B.", batch: "Hash", dept: "Scholastics", bio: "BIO GOES HERE", exp: "EXPERTISE GO HERE", interests: "INTERESTS GO HERE"},
+    {id: 1, image: "/assets/Snorlax.png", name: "POKEMON, SNORLAX B.", batch: "Hash", dept: "Scholastics", bio: "BIO GOES HERE", exp: "EXPERTISE GO HERE", interests: "INTERESTS GO HERE"},
+    {id: 1, image: "/assets/Snorlax.png", name: "POKEMON, SNORLAX B.", batch: "Hash", dept: "Scholastics", bio: "BIO GOES HERE", exp: "EXPERTISE GO HERE", interests: "INTERESTS GO HERE"},
 ]
 
 export default function Mentor(){
     return(
         <>  
-            <div style={{backgroundColor: '#132B41'}}>
-                {mentors.map((mentor) => (
-                    <CardMentor
-                        key = {mentor.id}
-                        image = {mentor.image}
-                        name = {mentor.name}
-                        batch = {mentor.batch}
-                        dept = {mentor.dept}
-                        bio = {mentor.bio}
-                        exp = {mentor.exp}
-                        interests = {mentor.interests}
-                    />
-                ))}
+            <div className="mentor-page">
+
+                <div className="mentor-main">
+                    <div className="mentor-top-picks">
+                        <div className="mentor-top-components">
+                            {mentors.slice(0, 3).map((mentor) => (
+                                <CardMentor
+                                    key={mentor.id}
+                                    image={mentor.image}
+                                    name={mentor.name}
+                                    batch={mentor.batch}
+                                    dept={mentor.dept}
+                                    bio={mentor.bio}
+                                    exp={mentor.exp}
+                                    interests={mentor.interests}
+                                />
+                            ))}
+                        </div>
+
+                        <div className="mentor-top-text">
+                        ⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯ TOP RECOMMENDED MENTORS FOR YOU ⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯
+                        </div>
+                    </div>
+                </div>
+
+                <div className="other-mentors-container">
+                    <div className="other-mentors">
+                            {mentors.map((mentor) => (
+                                <CardMentor
+                                    key = {mentor.id}
+                                    image = {mentor.image}
+                                    name = {mentor.name}
+                                    batch = {mentor.batch}
+                                    dept = {mentor.dept}
+                                    bio = {mentor.bio}
+                                    exp = {mentor.exp}
+                                    interests = {mentor.interests}
+                                />
+                            ))}
+                    </div>
+                </div>
+
             </div>
-            
         </>
     )
 }
